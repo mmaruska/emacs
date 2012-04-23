42fb620e69776079920654750ebba8eef35e9f6f
Michal Maruska <mmaruska@gmail.com>
2012-04-23 17:53:55 +0200

    dropping part: conflicts with C-g inside minibuffer.

--- src/process.c
+++ src/process.c
@@ -4798,8 +4798,9 @@ wait_reading_process_output (int time_limit, int microsecs, int read_kbd,
 	 obey it now if we should.  */
       if (read_kbd || ! NILP (wait_for_cell))
 	do_pending_window_change (0);
-#if 1                           /* mmc: I decided to test with this: (new)*/
+#if 0 /* mmc: I decided to test with this: (new)*/
       /* mmc: I need to redisplay if any emacs-window is damaged */
+      /* 2012-04-23 this now breaks (C-g inside minibuffer) */
       if (do_display)
         redisplay_preserve_echo_area (12);
 #endif
