05b0dbd661c2ad043d2d2c049ce15cdbd97a982c
Michal Maruska <mmaruska@gmail.com>
2012-04-25 01:11:31 +0200

    Don't auto-raise: ignore Enter/Leave Notify.

--- src/xterm.c
+++ src/xterm.c
@@ -4780,7 +4780,8 @@ x_detect_focus_change (struct x_display_info *dpyinfo, struct frame *frame,
           = focus_frame ? focus_frame->output_data.x->focus_state : 0;
 
         if (event->xcrossing.detail != NotifyInferior
-            && event->xcrossing.focus
+            && event->xcrossing.focus /* The window is the focused one! */
+            && focus_frame
             && ! (focus_state & FOCUS_EXPLICIT))
           x_focus_changed ((event->type == EnterNotify ? FocusIn : FocusOut),
 			   FOCUS_IMPLICIT,
